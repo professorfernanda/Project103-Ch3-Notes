309f5fa72e247bb16cdde19344988f52d1e9d74a
professorfernanda <fmurillo@sdgku.edu>
2025-09-25 15:44:13 -0700

    Initial Commit

---
+++ Project103-Ch3-Notes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project103-Ch3-Notes
+//
+//  Created by Fernanda Murillo on 25/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project103-Ch3-Notes/Project103_Ch3_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project103_Ch3_NotesApp.swift
+//  Project103-Ch3-Notes
+//
+//  Created by Fernanda Murillo on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Project103_Ch3_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
